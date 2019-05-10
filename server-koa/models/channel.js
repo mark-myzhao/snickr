50399b544cb9751becb21546d2d5ace87434aa44
colleoniF <835682342@qq.com>
2019-05-10 14:28:00 -0400

    channel update

--- server-koa/models/channel.js
+++ server-koa/models/channel.js
@@ -2,7 +2,7 @@ const db = require('../db.js')
 
 let getchannelinfo = async (wid) => {
   try {
-    let data = await db.query('SELECT cname, ctype, ctime FROM Channel WHERE wid = ?', wid)
+    let data = await db.query('SELECT Channel.cname, ctype, cMember.uemail AS oemail, uname As oname, ctime FROM Channel NATURAL JOIN cMember NATURAL JOIN User WHERE wid = ?', wid)
     return data
   } catch (error) {
     throw error
